cf6533689812140451cf20706a43ba29d9bd5460
surbhi <surbhirawat96@gmail.com>
2017-04-13 05:51:39 +0530

    second commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,11 +10,17 @@ $('document').ready(function(){
 
        var length = newNotes.length;
 
+       var countStudyMaterial = 0;
+       var countQuestionPaper = 0;
+       var countProjectReport = 0;
+
        for (var i = 0; i<length; i++)
        {
         var noteObject = newNotes[i];
 
-        var sno = i + 1;
+
+
+
         var notesName = noteObject.doc_name;
         var description = noteObject.doc_description;
         var subject = noteObject.subject.subject;
@@ -22,21 +28,24 @@ $('document').ready(function(){
 
          var type= noteObject.document_type;
 
-        // if(type =='study Material')
-        // {
-            $('#studyMaterials tbody').append('<tr> <td> '+sno+' </td> <td> '+notesName+' </td> <td> '+description+' </td> <td> '+subject+' </td> <td><a target="_blank" href="'+url+'"> download</a></td> </tr>');
-       //}
-        //
-        // else if(type == 'question Papers')
-        // {
-        //     $('#questionPapers tbody').append('<tr> <td> '+sno+' </td> <td> '+notesName+' </td> <td> '+description+' </td> <td> '+subject+' </td> <td><a target="_blank" href="'+url+'"> download</a></td> </tr>');
-        // }
-        //
-        // else if(type == 'project Report')
-        // {
-        //     $('#projectReports tbody').append('<tr> <td> '+sno+' </td> <td> '+notesName+' </td> <td> '+description+' </td> <td> '+subject+' </td> <td><a target="_blank" href="'+url+'"> download</a></td> </tr>');
-        // }
-        //
+        if(type =='Study Material')
+        {
+            countStudyMaterial++;
+            $('#studyMaterials tbody').append('<tr> <td> '+countStudyMaterial+' </td> <td> '+notesName+' </td> <td> '+description+' </td> <td> '+subject+' </td> <td><a target="_blank" href="'+url+'"> download</a></td> </tr>');
+        }
+
+        else if(type == 'Project Report')
+        {
+            countProjectReport++;
+            $('#projectReports tbody').append('<tr> <td> '+countProjectReport+' </td> <td> '+notesName+' </td> <td> '+description+' </td> <td> '+subject+' </td> <td><a target="_blank" href="'+url+'"> download</a></td> </tr>');
+        }
+
+        else if(type == 'Question Papers')
+        {
+            countQuestionPaper++;
+            $('#questionPapers tbody').append('<tr> <td> '+countQuestionPaper+' </td> <td> '+notesName+' </td> <td> '+description+' </td> <td> '+subject+' </td> <td><a target="_blank" href="'+url+'"> download</a></td> </tr>');
+        }
+
 
 
       }
