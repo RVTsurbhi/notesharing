3a23eefe1544b4cba0e2d53de5a34e782f1da6aa
surbhi <surbhirawat96@gmail.com>
2017-04-15 07:22:43 +0530

    upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,148 @@
 $('document').ready(function(){
-  $.ajax({
-    url: "http://acadprojects.com/py/notes/sharing/note",
-    type:"GET",
-    success: function(result){
-      console.log(result);
 
-       var newNotes = result.notes;
-       console.log(newNotes);
+  var latestUrl = "http://acadprojects.com/py/notes/sharing/note";
+  getNotes(latestUrl);
 
-       var length = newNotes.length;
+  function getNotes(postUrl)
+  {
+    $.ajax({
+      url: postUrl,
+      type:"GET",
+      success: function(result){
+        console.log(result);
 
-       var countStudyMaterial = 0;
-       var countQuestionPaper = 0;
-       var countProjectReport = 0;
+        $("#studyMaterials tbody").empty();
+        $("#questionPapers tbody").empty();
+        $("#projectReports tbody").empty();
 
-       for (var i = 0; i<length; i++)
-       {
-        var noteObject = newNotes[i];
+         var newNotes = result.notes;
+         console.log(newNotes);
+
+         var length = newNotes.length;
+
+         var countStudyMaterial = 0;
+         var countQuestionPaper = 0;
+         var countProjectReport = 0;
+
+         for (var i = 0; i<length; i++)
+         {
+          var noteObject = newNotes[i];
 
 
 
 
-        var notesName = noteObject.doc_name;
-        var description = noteObject.doc_description;
-        var subject = noteObject.subject.subject;
-        var url = noteObject.doc_url;
+          var notesName = noteObject.doc_name;
+          var description = noteObject.doc_description;
+          var subject = noteObject.subject.subject;
+          var url = noteObject.doc_url;
 
-         var type= noteObject.document_type;
+           var type= noteObject.document_type;
 
-        if(type =='Study Material')
-        {
-            countStudyMaterial++;
-            $('#studyMaterials tbody').append('<tr> <td> '+countStudyMaterial+' </td> <td> '+notesName+' </td> <td> '+description+' </td> <td> '+subject+' </td> <td><a target="_blank" href="'+url+'"> download</a></td> </tr>');
+          if(type =='Study Material')
+          {
+              countStudyMaterial++;
+              $('#studyMaterials tbody').append('<tr> <td> '+countStudyMaterial+' </td> <td> '+notesName+' </td> <td> '+description+' </td> <td> '+subject+' </td> <td><a target="_blank" href="'+url+'"> download</a></td> </tr>');
+          }
+
+          else if(type == 'Project Report')
+          {
+              countProjectReport++;
+              $('#projectReports tbody').append('<tr> <td> '+countProjectReport+' </td> <td> '+notesName+' </td> <td> '+description+' </td> <td> '+subject+' </td> <td><a target="_blank" href="'+url+'"> download</a></td> </tr>');
+          }
+
+          else if(type == 'Question Papers')
+          {
+              countQuestionPaper++;
+              $('#questionPapers tbody').append('<tr> <td> '+countQuestionPaper+' </td> <td> '+notesName+' </td> <td> '+description+' </td> <td> '+subject+' </td> <td><a target="_blank" href="'+url+'"> download</a></td> </tr>');
+          }
+
+
+
         }
 
-        else if(type == 'Project Report')
+      }});
+  }
+
+
+
+    $('#arts').on('click',function(){
+
+      $("#bottomnavbar ul li").removeClass('active');
+      $(this).parent().addClass('active');
+
+      var url = "http://acadprojects.com/py/notes/sharing/note?category=arts";
+      getNotes(url);
+    });
+
+
+    $('#engineering').on('click',function(){
+      $("#bottomnavbar ul li").removeClass('active');
+      $(this).parent().addClass('active');
+
+      var url = "http://acadprojects.com/py/notes/sharing/note?category=engineering";
+      getNotes(url);
+    });
+
+
+    $('#science').on('click',function(){
+      $("#bottomnavbar ul li").removeClass('active');
+      $(this).parent().addClass('active');
+
+      var url = "http://acadprojects.com/py/notes/sharing/note?category=science";
+      getNotes(url);
+    });
+
+    $('#maths').on('click',function(){
+      $("#bottomnavbar ul li").removeClass('active');
+      $(this).parent().addClass('active');
+
+      var url = "http://acadprojects.com/py/notes/sharing/note?category=mathematics";
+      getNotes(url);
+    });
+
+
+    $('#latest').on('click',function(){
+      $("#bottomnavbar ul li").removeClass('active');
+      $(this).parent().addClass('active');
+
+      var url = "http://acadprojects.com/py/notes/sharing/note";
+      getNotes(url);
+    });
+
+
+
+    $("#submitBtn").on('click',function(){
+      var docName = $("#fileName").val();
+      var docDescription = $("#description").val();
+      var subject = $("#subject").val();
+      var documentType = $("#documentType").val();
+
+      var myFile = $("input[type=file]")[0].files[0];
+
+      var formdata = new FormData();
+
+      formdata.append("file", myFile);
+      formdata.append("doc_description",docDescription);
+      formdata.append("document_type", documentType);
+      formdata.append("doc_name", docName);
+      formdata.append("category", subject);
+
+
+      $.ajax({
+        url:'http://acadprojects.com/py/notes/sharing/note',
+        data: formdata,
+        type: 'POST',
+        contentType: false,
+        processData: false,
+        success: function(result)
         {
-            countProjectReport++;
-            $('#projectReports tbody').append('<tr> <td> '+countProjectReport+' </td> <td> '+notesName+' </td> <td> '+description+' </td> <td> '+subject+' </td> <td><a target="_blank" href="'+url+'"> download</a></td> </tr>');
+          alert("submitted");
+          location.reload();
         }
+      });
 
-        else if(type == 'Question Papers')
-        {
-            countQuestionPaper++;
-            $('#questionPapers tbody').append('<tr> <td> '+countQuestionPaper+' </td> <td> '+notesName+' </td> <td> '+description+' </td> <td> '+subject+' </td> <td><a target="_blank" href="'+url+'"> download</a></td> </tr>');
-        }
+    });
 
 
 
-      }
-
-    }});
-  });
+ });
